1cfa8192324fe0809b2411239e68ad1b9332bb09
Juha-Pekka Ruuska <jpruuska@gmail.com>
2024-10-19 08:58:25 +0300

    Ready for user tests

--- hiking_mear_manager.py
+++ hiking_mear_manager.py
@@ -8,6 +8,10 @@ class App(tk.Tk):
         self.geometry("1400x550")
         self.base_inventory = cl.BaseInventory()
         self.packed_inventory = cl.PackedInventory()
+        self.total_weight = 0
+        self.total_items = 0
+        self.messagebox = None
+        
 
         # Main header
         self.label = tk.Label(self, text="Enter the gear info:")
@@ -35,7 +39,7 @@ class App(tk.Tk):
         self.item_weight_entry.grid(row=1, column=5)
 
         # Add to base inventory button
-        self.add_item_button = tk.Button(self, text="Add to base inventory", command=self.add_item)
+        self.add_item_button = tk.Button(self, text="Add to base inventory", command=self.add_and_save)
         self.add_item_button.grid(row=1, column=6, columnspan=2)
 
         # Headers for listboxes
@@ -54,24 +58,28 @@ class App(tk.Tk):
         self.packed_items_listbox.grid(row=6, column=7, columnspan=7, sticky="w", pady=(10, 0))
 
         # Remove from inventory button
-        self.remove_item_button = tk.Button(self, text="Remove from inventory", command=self.remove_from_base_inventory)
+        self.remove_item_button = tk.Button(self, text="Remove from inventory", command=self.remove_and_save)
         self.remove_item_button.grid(row=7, column=0, columnspan=2, padx=(20, 20))
 
         # Pack item button
-        self.pack_item_button = tk.Button(self, text="Pack item \u2192", command=self.pack_item)
+        self.pack_item_button = tk.Button(self, text="Pack item \u2192", command=self.press_pack_button)
         self.pack_item_button.grid(row=7, column=3, columnspan=2)
 
         # Unpack item button
-        self.unpack_item_button = tk.Button(self, text="Unpack item \u00D7", command=self.unpack_item)
+        self.unpack_item_button = tk.Button(self, text="Unpack item \u00D7", command=self.press_unpack_button)
         self.unpack_item_button.grid(row=7, column=7, columnspan=2)
 
-        # Placeholder for total amount and weight display
-        self.total_display_label = tk.Label(self, text="Total items: 0, Total weight: 0g")
+        # Total amount and weight display
+        self.total_display_label = tk.Label(self, text=f"Total items: {self.total_items}, Total weight: {self.total_weight}g")
         self.total_display_label.grid(row=8, column=8, columnspan=14, pady=(10, 0))
 
         # Save base inventory button
-        self.save_base_inventory_button = tk.Button(self, text="Save base inventory", command=self.save_base_inventory)
-        self.save_base_inventory_button.grid(row=9, column=0, columnspan=2, pady=(10, 0), padx=(20, 20))
+        #self.save_base_inventory_button = tk.Button(self, text="Save base inventory", command=self.save_base_inventory)
+        #self.save_base_inventory_button.grid(row=9, column=0, columnspan=2, pady=(10, 0), padx=(20, 20))
+
+        # Messagebox placeholder
+        self.messagebox = tk.Label(self, text=self.messagebox)
+        self.messagebox.grid(row=9, column=0, columnspan=2, pady=(10, 0), padx=(20, 20))
 
         # Save packed items button
         self.save_packed_items_button = tk.Button(self, text="Save packed items", command=self.save_packed_items_inventory)
@@ -88,12 +96,18 @@ class App(tk.Tk):
         item_category = self.item_category_entry.get()
         item_weight = self.item_weight_entry.get()
 
-        item = cl.Item(item_name, item_category, item_weight)
-        self.base_inventory_listbox.insert(tk.END, item)
+        if not item_name or not item_category or not item_weight:
+            self.messagebox.config(text="Please fill all fields")
+        elif not item_weight.isdigit():
+            self.messagebox.config(text="Weight must be a number")
+        else:
+            item = cl.Item(item_name, item_category, item_weight)
+            self.base_inventory_listbox.insert(tk.END, item)
 
-        self.item_name_entry.delete(0, tk.END)
-        self.item_category_entry.delete(0, tk.END)
-        self.item_weight_entry.delete(0, tk.END)
+            self.item_name_entry.delete(0, tk.END)
+            self.item_category_entry.delete(0, tk.END)
+            self.item_weight_entry.delete(0, tk.END)
+            self.messagebox.config(text="Item added successfully")
 
     # function to remove selected item from base inventory
     def remove_from_base_inventory(self):
@@ -118,11 +132,30 @@ class App(tk.Tk):
         
     # function to update total weight
     def update_total_weight(self):
-        pass
+        self.total_weight = 0
+        for i in range(self.packed_items_listbox.size()):
+            item_str: str = self.packed_items_listbox.get(i)
+            name, category, weight = item_str.split(",")
+            self.total_weight += int(weight[:-1])
+        
+        
 
     # function to update total amount of items
     def update_total_items(self):
-        pass
+        self.total_items = self.packed_items_listbox.size()
+        
+    
+    def press_pack_button(self):
+        self.pack_item()
+        self.update_total_weight()
+        self.update_total_items()
+        self.total_display_label.config(text=f"Total items: {self.total_items}, Total weight: {self.total_weight}g")
+
+    def press_unpack_button(self):
+        self.unpack_item()
+        self.update_total_weight()
+        self.update_total_items()
+        self.total_display_label.config(text=f"Total items: {self.total_items}, Total weight: {self.total_weight}g")
 
 
     """functions to run at the start of the app"""
@@ -132,22 +165,22 @@ class App(tk.Tk):
         for item in self.base_inventory.get_items():
             self.base_inventory_listbox.insert(tk.END, item)
         self.base_inventory.remove_all_items()
-        
+
 
     # funktion to load items to packed items display
     def load_packed_items_inventory_display(self):
         for item in self.packed_inventory.get_items():
             self.packed_items_listbox.insert(tk.END, item)
         self.packed_inventory.remove_all_items()
+        self.update_total_weight()
+        self.update_total_items()
+        self.total_display_label.config(text=f"Total items: {self.total_items}, Total weight: {self.total_weight}g")
         
 
     def start(self):
         self.load_base_inventory_display()
         self.load_packed_items_inventory_display()
         
-
-    """functions to run at the end of the app"""
-    
 
     # function to save base inventory items to list -> save to file
     def save_base_inventory(self):
@@ -170,21 +203,14 @@ class App(tk.Tk):
         self.packed_inventory.save_file(self.packed_inventory.get_items())
         self.packed_inventory.remove_all_items()
 
-
-        
-        
-        # lisää myös syötteiden tarvittava tarkistus
-        # lisää myös total painon ja itemien määrän päivitys
-        # inventoryn tallennuksessa lista pitäisi tallentua sellaisena kuin se on listboxissa. Jos item poistettu listboxista, pitäisi se poistaa myös inventoryn itemeistä
-
     
+    def add_and_save(self):
+        self.add_item()
+        self.save_base_inventory()
 
-        
-
-       
-
-
-
+    def remove_and_save(self):
+        self.remove_from_base_inventory()
+        self.save_base_inventory()
 
 
 
